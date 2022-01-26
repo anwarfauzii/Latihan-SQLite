db820a34088f67ec3f4e2bffdceafb9f15595f1a
Anwar_Fauzi <Anwarfauzi2608@gmail.com>
2022-01-26 14:36:17 +0700

    delete unused folder

--- lib_backup/src/common/app_constant.dart
+++
@@ -1,6 +0,0 @@
-class AppConstant{
-  static const String dbName = 'my_database.db';
-  static const String tableName = 'name';
-  static const String fId = 'id';
-  static const String fContent = 'fullname';
-}
